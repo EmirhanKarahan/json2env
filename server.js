3e94cfd15f0d770fd59f327b8f7106be6dcf7160
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-09-01 11:58:12 +0300

    Add server configurations

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, 'public')
+
+express()
+  .use(express.static(publicPath))
+  .get("*", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
+  })
+  .listen(port, () => {
+    console.log("Server is up, " + port);
+  })
